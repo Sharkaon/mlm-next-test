29d8b6d4c1aa672c4ab499e8e4e1fe9e79120f5c
Artur "Shark" Monteiro <rutrakpm@gmail.com>
2024-03-23 21:59:29 -0300

    fix(ui): ajustada estilizacao e disposicao de dados

--- app/gemini/page.tsx
+++ app/gemini/page.tsx
@@ -1,5 +1,16 @@
-import messages from "@/messages";
+import Chat from '@/components/chat';
+import messages, { MessageInterface } from "@/messages";
 
 export default function GeminiExample() {
-  return <>To Do</>
+  const googleMessages: MessageInterface = messages.map(
+    (message) => message.role === 'system' ? {
+      ...message,
+      role: 'assistant' as const,
+    } : message
+  );
+
+  return <>
+    Gemini<br/>
+    <Chat firstMessage={googleMessages[0]} userDefaultMessage={googleMessages[1].content} />
+  </>
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -2,8 +2,8 @@
 
 import { getCompletion } from "@/app/open-ai/actions";
 import { MessageInterface } from "@/messages";
-import { useState } from "react";
-import { useFormState } from "react-dom";
+import { useEffect, useState } from "react";
+import { useFormState, useFormStatus } from "react-dom";
 
 export type ChatProps = {
   firstMessage: MessageInterface;
@@ -13,38 +13,82 @@ export type ChatProps = {
 export default function Chat(props: ChatProps) {
   const [state, formAction] = useFormState(getCompletion, '');
   const [messages, setMessages] = useState([props.userDefaultMessage ?? '']);
+  const [waitingResponse, setWaitingRespone] = useState(false);
+  const formStatus = useFormStatus();
+
+  useEffect(() => {
+    console.log(state);
+    if (!waitingResponse) return;
+
+    addResult(state);
+    setWaitingRespone(false);
+  }, [state]);
 
   const addResult = (result: string) => {
     setMessages(messages.concat(result).concat(''));
   }
 
   return <div className='flex flex-col items-center'>
-    <form action={formAction} className="h-80">
-      <h4>A primeira mensagem é usada apenas para contextualizar o modelo de qual o seu papel para o usuário</h4>
+    <form action={formAction} className="h-80 flex flex-col w-4/5" onSubmit={() => setWaitingRespone(true)}>
+      <h4>A primeira mensagem é usada apenas para contextualizar o modelo de qual o seu papel para o usuário. Ela tem o role de {props.firstMessage.role}</h4>
       <textarea
         name='message'
         value={props.firstMessage.content}
-        className='text-black w-full text-sm mb-3 rounded'
+        readOnly
+        className='w-full text-sm mb-3 rounded border-none bg-gray-500 text-white cursor-text'
       />
 
       <h4>Após isso ela é intercalada por uma mensagem de usuário (escrita pelo usuário) e uma mensagem de assistente (que apenas reenvia o que o modelo já falou para que ele tenha o contexto da conversa)</h4>
       {messages.map(
         (message, index) =>
-          <textarea
+          index % 2 === 0 ? <UserInput
             key={index}
-            name='message'
-            defaultValue={message}
-            className={`text-black w-full text-sm mb-3 rounded`}
-            readOnly={index % 2 !== 0}
+            message={message}
+          />: <ResponseInput
+            key={index}
+            message={message}
           />
       )}
 
-      {state !== '' && <button
-        className='bg-gray-800 mb-2 rounded-md text-lg pl-2 pr-2'
-        onClick={() => addResult(state)}
-      >+</button>} {state}<hr/>
-
-      <button type='submit'>Próxima</button>
+      {
+        waitingResponse ? <p>Carregando...</p>
+        : <button
+            disabled={formStatus.pending}
+            type='submit'
+            className={
+              `${formStatus.pending && 'text-gray-300'}`
+            }
+        >Próxima</button>
+      }
     </form>
   </div>
+}
+
+const UserInput = (props: {
+  message: string
+}) => {
+  const {
+    message,
+  } = props;
+
+  return <textarea
+    name='message'
+    defaultValue={message}
+    className={`text-black w-11/12 text-sm mb-3 rounded`}
+  /> 
+}
+
+const ResponseInput = (props: {
+  message: string
+}) => {
+  const {
+    message,
+  } = props;
+
+  return <textarea
+    name='message'
+    defaultValue={message}
+    className={`text-white w-11/12 text-sm mb-3 rounded bg-blue-600 self-end`}
+    readOnly
+  />
 }
